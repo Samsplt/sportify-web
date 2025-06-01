f71042347f42b5af160923ba9161acbd9dadee5c
Samsplt <150029884+Samsplt@users.noreply.github.com>
2025-06-01 19:31:42 +0200

    Create Footer.php

---
+++ Footer.php
@@ -0,0 +1,12 @@
+<footer class="bg-dark text-white text-center py-4 mt-5">
+  <div><i class="bi bi-geo-alt-fill me-1"></i>Adresse : 13 rue Seixtus Michel, 75015 Paris</div>
+  <div><i class="bi bi-telephone-fill me-1"></i>01 23 45 67 89 | <i class="bi bi-envelope-fill me-1"></i>sportify@omnessport.fr</div>
+  <div class="mt-2">©️ 2025 <strong>WEB DYNAMIQUE</strong></div>
+</footer>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<!-- JS  -->
+<script src="js/script.js"></script>
+</body>
+</html>
