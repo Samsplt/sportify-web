f139432a592a01d0236d48238c4b0d961a507e57
Alexandre <91475075+alexandrevdv@users.noreply.github.com>
2025-06-01 19:23:49 +0200

    Create services_salle.php

---
+++ services_salle.php
@@ -0,0 +1,15 @@
+<?php include 'header.php'; ?>
+
+<div class="container mt-5 text-center">
+    <h2 class="mb-4">Nos services</h2>
+
+    <div class="d-grid gap-3 col-6 mx-auto">
+        <a href="personnel_salle.php" class="btn btn-outline-primary">Personnels de la salle de sport</a>
+        <a href="horaire_salle.php" class="btn btn-outline-primary">Horaire de la gym</a>
+        <a href="regles_machines.php" class="btn btn-outline-primary">Règles sur l’utilisation des machines</a>
+        <a href="nouveaux_clients.php" class="btn btn-outline-primary">Nouveaux clients</a>
+        <a href="nutrition.php" class="btn btn-outline-primary">Alimentation et nutrition</a>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
